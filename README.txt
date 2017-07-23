a0273ef18062f0f21ce691f606961d36592181b3
Sean Carpenter <Seancarpenter10@gmail.com>
2017-07-23 14:29:53 -0400

    Added simple user interaction

--- README.txt
+++ README.txt
@@ -1,3 +1,14 @@
 Word Jolt
 
-A web application designed with the intention of allowing users to make very fast English dictionary queries by utilizing tries.
+A web application designed with the intention of allowing users to make fast English dictionary queries by utilizing tries.
+
+
+Development Roadmap
+
+1. Build Core Application:
+	- Capable of generating 
+2. Build Django website 
+3. Host Django Website
+4. Add support for defintions
+5. Add support for different languages
+6. Android App
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from trie import Trie
 import time
+import sys
 def main():
 	start = time.time()
 	words = []
 	with open("languages/english.txt") as file:
 		words = file.read().splitlines()
 	t = Trie(words)
-	start = time.time()
-	print(t.same_prefix("cat")[:10])
-	print(time.time() - start)
 
+	print("Type \"exit()\" to quit the program.")
+	while(True):
+		word_input = input("Enter a word: ")
+		if word_input == "exit()":
+			sys.exit()
+		for word in t.same_prefix(word_input):
+			print(word)
 main()
